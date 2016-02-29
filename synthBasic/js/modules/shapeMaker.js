e063b53a3a61fc5b9f56f84bf367616d0ccbed1b
william-bratches <williambratches@gmail.com>
2016-02-28 23:20:12 -0500

    started on octave generator

--- synthBasic/js/modules/shapeMaker.js
+++ synthBasic/js/modules/shapeMaker.js
@@ -1,16 +1,29 @@
 Modules.ShapeMaker = (function() {
+  var materialCreator = new THREE.MTLLoader.MaterialCreator();
+  materialCreator.crossOrigin = 'anonymous';
+
+
   function createCube(scale, color) {
     var scaler = scale || 50;
-    var color = color || '#ffffff'
-    var materialCreator = new THREE.MTLLoader.MaterialCreator();
+    var color = color || '#ffffff';
   	var geometry = new THREE.BoxGeometry(1, 1, 1);
   	var material = new THREE.MeshBasicMaterial({color: color});
   	var cube = new THREE.Mesh(geometry, material);
-    materialCreator.crossOrigin = 'anonymous';
     cube.scale.multiplyScalar(scaler);
     return cube;
   }
+
+  function createWhiteKey(index) {
+    var octaveIndex = index || 0;
+    var color = '#ffffff';
+    var geometry = new THREE.BoxGeometry(1, 1, 5);
+    var material = new THREE.MeshBasicMaterial({color: color});
+    var whiteKey = new THREE.Mesh(geometry, material);
+    whiteKey.scale.multiplyScalar(20);
+    return whiteKey;
+  }
   return {
+    createWhiteKey: createWhiteKey,
     createCube: createCube,
   }
 })();
