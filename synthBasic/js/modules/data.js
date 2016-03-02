80087c8e41993d721b7c0641e14b564c674392a9
william-bratches <williambratches@gmail.com>
2016-03-02 00:26:21 -0500

    added notes matrix

--- synthBasic/js/modules/data.js
+++ synthBasic/js/modules/data.js
@@ -1,10 +1,19 @@
 Modules.Data = (function() {
 
-  function notesTable() {
-    
+  function notesMatrix() {
+    // starts at F for each octave
+    var matrix = [
+      [43.654, 48.999, 55, 61.735, 65.406,73.416, 82.407],
+      [87.307, 97.999, 110, 123.47, 130.81, 146.83, 164.81],
+      [174.61, 196, 220, 246.94, 261.63, 293.67, 329.63],
+      [349.23, 392, 440, 493.88, 523.25, 587.33, 659.26],
+      [698.46, 783.99, 880, 987.77, 1046.5, 1174.7, 1318.5],
+      [1396.9, 1568, 1760, 1975.5, 2093, 2349.3, 2637]
+    ]
+    return matrix;
   }
 
   return {
-    notesTable: notesTable;
+    notesMatrix: notesMatrix;
   }
 })();
